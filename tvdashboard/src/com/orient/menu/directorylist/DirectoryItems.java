1783acef5b5b6b69ca846b96825f4fca9c091158
Shahzeb <shahzeb.rahat@offroadstudios.com>
2014-01-30 13:44:59 +0500

    dashboard changed

---
+++ tvdashboard/src/com/orient/menu/directorylist/DirectoryItems.java
@@ -0,0 +1,20 @@
+package com.orient.menu.directorylist;
+
+public class DirectoryItems {
+	
+	private String title;
+
+	public DirectoryItems(String title) {
+		super();
+		this.title = title;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+}
