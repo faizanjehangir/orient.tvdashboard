c63bfef4bdb3bc271dc19272fd12d69e5af82ff2
Shahzeb <shahzeb.rahat@offroadstudios.com>
2014-02-05 19:34:29 +0500

    tvdashboard changes 5 feb 14

---
+++ tvdashboard/src/com/tvdashboard/main/VideosFragment.java
@@ -0,0 +1,39 @@
+package com.tvdashboard.main;
+
+import com.tvdashboard.database.R;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class VideosFragment extends Fragment{
+	
+	public static VideosFragment newInstance() {
+        VideosFragment fragment = new VideosFragment();
+
+        return fragment;
+    }
+
+	@Override
+	public void onActivityCreated(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onActivityCreated(savedInstanceState);
+	}
+
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+	}
+
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup container,
+			Bundle savedInstanceState) {
+		View view = inflater.inflate(R.layout.videos_fragment_layout, container, false);
+		return view;
+	}	
+	
+
+}
